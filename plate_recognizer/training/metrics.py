8c140d85e0ac70bd0396778c88248b924fa527da
changsin <changsin@hotmail.com>
2021-06-21 08:29:30 +0000

    calc_map fix

--- plate_recognizer/training/metrics.py
+++ plate_recognizer/training/metrics.py
@@ -278,16 +278,16 @@ def calc_iou(gt_bbox, pred_bbox):
       return 0.0
     
     GT_bbox_area = (x_bottomright_gt - x_topleft_gt + 1) * (y_bottomright_gt - y_topleft_gt + 1)
-    Pred_bbox_area =(x_bottomright_p - x_topleft_p + 1) * (y_bottomright_p - y_topleft_p + 1)
+    Pred_bbox_area = (x_bottomright_p - x_topleft_p + 1) * (y_bottomright_p - y_topleft_p + 1)
     
-    x_top_left =np.max([x_topleft_gt, x_topleft_p])
+    x_top_left = np.max([x_topleft_gt, x_topleft_p])
     y_top_left = np.max([y_topleft_gt, y_topleft_p])
     x_bottom_right = np.min([x_bottomright_gt, x_bottomright_p])
     y_bottom_right = np.min([y_bottomright_gt, y_bottomright_p])
     
     intersection_area = (x_bottom_right - x_top_left + 1) * (y_bottom_right - y_top_left  + 1)
     
-    union_area = (GT_bbox_area + Pred_bbox_area - intersection_area)
+    union_area = float(GT_bbox_area + Pred_bbox_area - intersection_area)
    
     return intersection_area/union_area
 
